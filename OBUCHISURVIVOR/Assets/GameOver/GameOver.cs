3bfc84385cf8c79b8300ea9036509b8971c036e2
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-08-24 17:12:56 +0900

    up

--- OBUCHISURVIVOR/Assets/GameOver/GameOver.cs
+++ OBUCHISURVIVOR/Assets/GameOver/GameOver.cs
@@ -1,8 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    
+    public GameObject returnGameBt;
+    public GameObject returnTitleBt;
+
+    int stageNo;
+    bool isStart = false;
+    bool isGame = false;
+
+    float alpha = 1.0f;
+
+    void Start()
+    {
+        stageNo = PlayerPrefs.GetInt("StageNo", 1);
+    }
+
+    void FixedUpdate()
+    {
+        if (isStart)
+        {
+            // フェード処理
+            alpha -= 0.0625f;
+
+            if (alpha <= 0)
+            {
+                if (isGame)
+                {
+                    SceneManager.LoadScene("stage" + stageNo.ToString());
+                }
+                else
+                {
+                    SceneManager.LoadScene("Title");
+                }
+            }
+        }
+    }
+
+    public void ReturnGameBt()
+    {
+        isStart = true;
+        isGame = true;
+    }
+
+    public void ReturnTitleBt()
+    {
+        isStart = true;
+    }
 }
