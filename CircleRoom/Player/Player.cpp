0eba804491f144706f03dc63a4bb37d4a9139c04
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-11-18 23:07:13 +0900

    頭こんがらがるぜお

--- CircleRoom/Player/Player.cpp
+++ CircleRoom/Player/Player.cpp
@@ -20,7 +20,7 @@ namespace
 	constexpr int kDashWaitFrame = 25;
 
 	// 線形補間を行うフレーム
-	constexpr int kInterpolatedFrame = 5;
+	constexpr int kInterpolatedFrame = 50;
 }
 
 Player::Player(Application& app) :
@@ -62,13 +62,25 @@ void Player::Draw()
 		static_cast<int>(m_rightVec.x + m_pos.x), static_cast<int>(m_rightVec.y + m_pos.y),
 		0xffffff, true);
 
+	// プレイヤーの中心を表示
 	DrawCircle(static_cast<int>(m_pos.x), static_cast<int>(m_pos.y), 3, 0xff00ff, true);
 
-	DrawFormatString(32, 32, 0xffffff, L"%.2f, %.2f", m_pos.x, m_pos.y);
-	DrawFormatString(32, 32+16, 0xff0000, L"%.2f, %.2f", m_vec.x, m_vec.y);
-	DrawFormatString(32, 32+32, 0xff0000, L"%.2f, %.2f", m_nowFront.x, m_nowFront.y);
+	DrawFormatString(32, 32, 0xffffff, L"pos;%.2f, %.2f", m_pos.x, m_pos.y);
+	DrawFormatString(32, 32+16, 0xff0000, L"vec:%.2f, %.2f", m_vec.x, m_vec.y);
+	DrawFormatString(32, 32+32, 0xff0000, L"now:%.2f, %.2f", m_nowFront.x, m_nowFront.y);
 
-	
+	int centerX = m_size.w * 0.5f;
+	int centerY = m_size.h * 0.5f;
+	int lineLong = 30;
+	DrawLine(centerX, centerY,
+		centerX + m_lastChangeVec.x * lineLong, centerY + m_lastChangeVec.y * lineLong,
+		0x0000ff, 2);
+	DrawLine(centerX, centerY,
+		centerX + m_firstChangeVec.x * lineLong, centerY + m_firstChangeVec.y * lineLong,
+		0xff0000, 2);
+	DrawLine(centerX, centerY,
+		centerX + m_nowFront.x * lineLong, centerY + m_nowFront.y * lineLong,
+		0x00ff00, 2);
 }
 
 void Player::Move(Input& input)
@@ -98,7 +110,17 @@ void Player::Move(Input& input)
 	// 移動ベクトルを正規化
 	m_vec.Normalize();
 
+#if true
 	Lerp();
+#else
+	if (m_vec.SqLength() > 0)
+	{
+		m_nowFront = m_vec;
+		m_frontVec = m_nowFront * kSize;
+		m_rightVec = m_nowFront.Right() * kSize * 0.5f;
+		m_leftVec = m_nowFront.Left() * kSize * 0.5f;
+	}
+#endif
 
 	m_vec *= kSpeed;
 
@@ -113,12 +135,17 @@ void Player::Lerp()
 	// 動いていたら線形補間を更新する
 	if (m_vec != m_lastChangeVec && m_vec.SqLength() > 0)
 	{
+#if false
 		// 二点間の差＋2を補間にかかる時間とする
 		int num = abs(m_vec.x - m_nowFront.x + m_vec.y - m_nowFront.y)*5 + 2;
 		// 補間の時間＋元からあった補間の時間を入れる
 		m_interpolatedFrameNum = num + m_interpolatedFrame;
 		// 補間の時間を入れる
 		m_interpolatedFrame = num;
+#else
+		m_interpolatedFrameNum = kInterpolatedFrame;
+		m_interpolatedFrame = kInterpolatedFrame;
+#endif
 
 		m_firstChangeVec = m_nowFront;
 		m_lastChangeVec = m_vec;
@@ -129,6 +156,8 @@ void Player::Lerp()
 	// 現在の正面方向の更新
 	float rate = static_cast<float>(m_interpolatedFrame) / static_cast<float>(m_interpolatedFrameNum);
 	m_nowFront = m_lastChangeVec * (1.0f - rate) + m_firstChangeVec * (rate);
+
+	m_nowFront.Normalize();
 
 	m_frontVec = m_nowFront * kSize;
 	m_rightVec = m_nowFront.Right() * kSize * 0.5f;
