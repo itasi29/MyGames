058e45faf3ff80980ae5b607786e06b298641cbd
itasi29 <135081567+itasi29@users.noreply.github.com>
2024-04-16 16:53:10 +0900

    up

--- ShotTogether/Actor/ActorTarget.cpp
+++ ShotTogether/Actor/ActorTarget.cpp
@@ -1,1 +1,36 @@
 #include "ActorTarget.h"
+
+ActorTarget::ActorTarget(int handle, ActorManager& actorMgr, FileManager& fileMgr, SoundSystem& sndSys, const wchar_t* const name) :
+	ActorBase(handle, actorMgr, fileMgr, sndSys, name),
+	m_updateFunc(&ActorTarget::UpdateNeutual)
+{
+}
+
+ActorTarget::~ActorTarget()
+{
+}
+
+void ActorTarget::Init(float x, float y)
+{
+}
+
+void ActorTarget::Update(Input& input)
+{
+	(this->*m_updateFunc)(input);
+}
+
+void ActorTarget::OnDeath()
+{
+}
+
+void ActorTarget::UpdateNeutual(Input& input)
+{
+}
+
+void ActorTarget::UpdateDeath(Input& input)
+{
+	if (m_isAnimOneLoop)
+	{
+
+	}
+}
--- ShotTogether/Actor/ActorTarget.h
+++ ShotTogether/Actor/ActorTarget.h
@@ -10,9 +10,13 @@ public:
 	void Init(float x, float y) override;
 	void Update(Input& input) override;
 
+	void OnDeath();
+
 private:
 	using UpdateFunc_t = void(ActorTarget::*)(Input&);
 	void UpdateNeutual(Input& input);
+	void UpdateDeath(Input& input);
+
 
 private:
 	UpdateFunc_t m_updateFunc;
