84b60d1fc02250ee2550cfac90c17f3d5837803c
itasi29 <135081567+itasi29@users.noreply.github.com>
2024-04-16 15:37:01 +0900

    up

---
+++ ShotTogether/Actor/ActorTarget.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "ActorBase.h"
+
+class ActorTarget : public ActorBase
+{
+public:
+	ActorTarget(int handle, ActorManager& actorMgr, FileManager& fileMgr, SoundSystem& sndSys, const wchar_t* const name);
+	virtual ~ActorTarget();
+
+	void Init(float x, float y) override;
+	void Update(Input& input) override;
+
+private:
+	using UpdateFunc_t = void(ActorTarget::*)(Input&);
+	void UpdateNeutual(Input& input);
+
+private:
+	UpdateFunc_t m_updateFunc;
+};
+
