393a5296dca53d1aca99299c5216d0876ee9ad2d
itasi29 <135081567+itasi29@users.noreply.github.com>
2024-04-19 16:52:05 +0900

    up

---
+++ test3DCollison/Common/Application.cpp
@@ -0,0 +1,120 @@
+﻿#include "Application.h"
+#include <DxLib.h>
+#include "Game.h"
+#include "Geometry.h"
+
+namespace
+{
+    const wchar_t* const kWindowText = L"TempGame";
+}
+
+Application::Application() :
+    m_isEnd(false),
+#ifdef _DEBUG
+    m_isWindows(true)
+#else
+    m_isWindows(false)
+#endif
+{
+}
+
+Application& Application::GetInstance()
+{
+    static Application app;
+    return app;
+}
+
+bool Application::Init()
+{
+    ChangeWindowMode(m_isWindows);
+    SetChangeScreenModeGraphicsSystemResetFlag(false);
+
+    SetGraphMode(Game::kWindowWidth, Game::kWindowHeight, 32);
+    SetWindowText(kWindowText);
+
+    if (DxLib_Init() == -1) {
+        return false;
+    }
+    SetDrawScreen(DX_SCREEN_BACK);
+
+    SetUseZBuffer3D(true);
+    SetWriteZBuffer3D(true);
+
+    return true;
+}
+
+void Application::Run()
+{
+    auto cameraPos = VGet(0.0f, 10.0f, -100.0f);
+    auto targetPos = VGet(0.0f, 0.0f, 0.0f);
+
+    SetCameraNearFar(0.1f, 1000.0f);
+    SetCameraPositionAndTarget_UpVecY(cameraPos, targetPos);
+
+    int playerHandle = MV1LoadModel(L"Data/Model/player.mv1");
+    int enemyHandle = MV1LoadModel(L"Data/Model/enemy.mv1");
+
+    constexpr float kPlayerScale = 0.125f;
+    constexpr float kEnemyScale = 20.0f;
+
+    MV1SetScale(playerHandle, VGet(kPlayerScale, kPlayerScale, kPlayerScale));
+    MV1SetScale(enemyHandle, VGet(kEnemyScale, kEnemyScale, kEnemyScale));
+
+    Pos3 playerPos(50, -10.0f, 0.0f);
+    Pos3 enemyPos(-50.0f, -10.0f, 0.0f);
+
+    constexpr float kColRadius = 2.0f;
+
+    CircleCol playerCol;
+    CircleCol enemyCol;
+
+    playerCol.SetRadisu(kColRadius);
+    enemyCol.SetRadisu(kColRadius);
+
+
+    long long time;
+    while (ProcessMessage() != -1) {
+        time = GetNowHiPerformanceCount();
+
+        ClearDrawScreen();
+
+        constexpr float kSpeed = 0.1f;
+        if (CheckHitKey(KEY_INPUT_RIGHT))
+        {
+            playerPos.x += kSpeed;
+        }
+        if (CheckHitKey(KEY_INPUT_LEFT))
+        {
+            playerPos.x -= kSpeed;
+        }
+
+        MV1SetPosition(playerHandle, playerPos.GetVECTOR());
+        MV1SetPosition(enemyHandle, enemyPos.GetVECTOR());
+
+        playerCol.SetPos(playerPos);
+        enemyCol.SetPos(enemyPos);
+
+        MV1DrawModel(playerHandle);
+        MV1DrawModel(enemyHandle);
+        playerCol.Draw();
+        if (playerCol.IsHit(enemyCol))
+        {
+            enemyCol.Draw(0x00ff00);
+        }
+
+        ScreenFlip();
+
+        if (m_isEnd) {
+            break;
+        }
+        while (Game::kFpsFrame > GetNowHiPerformanceCount() - time);
+    }
+
+    MV1DeleteModel(playerHandle);
+    MV1DeleteModel(enemyHandle);
+}
+
+void Application::Terminate()
+{
+    DxLib_End();
+}
---
+++ test3DCollison/Common/Geometry.h
@@ -0,0 +1,162 @@
+#pragma once
+#include <DxLib.h>
+
+/// <summary>
+/// サイズ構造体
+/// </summary>
+struct Size
+{
+	// 幅
+	float w = 0.0f;
+	// 高さ
+	float h = 0.0f;
+	// 奥行き
+	float d = 0.0f;
+};
+
+/// <summary>
+/// ベクター構造体
+/// </summary>
+struct Vec3
+{
+	float x, y, z;
+
+	Vec3();
+	Vec3(float inX, float inY, float inZ);
+
+	Vec3 operator+(const Vec3& val) const;
+	void operator+=(const Vec3& val);
+	Vec3 operator-(const Vec3& val) const;
+	void operator-=(const Vec3& val);
+	Vec3 operator*(float scale) const;
+	void operator*=(float scale);
+	Vec3 operator/(float scale) const;
+	void operator/=(float scale);
+	bool operator==(const Vec3& val) const;
+	bool operator!=(const Vec3& val) const;
+
+	/// <summary>
+	/// DxLibの関数を使う際、VECTOR型に変換する関数
+	/// </summary>
+	/// <returns>VECTOR型の現在の値</returns>
+	VECTOR GetVECTOR() const { return VGet(x, y, z); }
+
+	/// <summary>
+	/// 長さを取得
+	/// </summary>
+	/// <returns>長さ</returns>
+	float Length() const;
+	/// <summary>
+	/// 長さの二乗を取得
+	/// </summary>
+	/// <returns>長さ</returns>
+	float SqLength() const;
+	/// <summary>
+	/// 自身の正規化
+	/// </summary>
+	void Normalize();
+	/// <summary>
+	/// 正規化した結果を取得
+	/// </summary>
+	Vec3 GetNormalized() const;
+};
+
+/// <summary>
+/// 線形補間
+/// </summary>
+/// <param name="start">スタート地点</param>
+/// <param name="end">終了地点</param>
+/// <param name="t">割合</param>
+/// <returns>補間後</returns>
+Vec3 Lerp(const Vec3& start, const Vec3& end, float t);
+
+using Pos3 = Vec3;
+
+/// <summary>
+/// 矩形の当たり判定(2D判定)
+/// 足元中心
+/// </summary>
+struct Rect
+{
+private:
+	// 位置
+	Pos3 pos;
+	// 幅高さ
+	float h, w, d;
+
+public:
+	Rect();
+
+	/// <summary>
+	/// 幅高さを設定
+	/// </summary>
+	/// <param name="inH">高さ</param>
+	/// <param name="inW">幅</param>
+	/// <param name="inD">奥行き</param>
+	void SetRange(float inH, float inW, float inD);
+	/// <summary>
+	/// 場所を設定
+	/// </summary>
+	/// <param name="inX">X座標</param>
+	/// <param name="inY">Y座標</param>
+	void SetPos(float inX, float inY);
+	/// <summary>
+	/// 場所を設定
+	/// </summary>
+	/// <param name="inPos">座標</param>
+	void SetPos(const Pos3& inPos);
+	/// <summary>
+	/// 衝突判定
+	/// </summary>
+	/// <param name="rect">判定側</param>
+	/// <returns>true:当たった / false:当たっていない</returns>
+	bool IsHit(const Rect& rect);
+	/// <summary>
+	/// 幅を取得
+	/// </summary>
+	/// <returns>width</returns>
+	float GetW() const { return w; }
+
+	/// <summary>
+	/// 判定場所描画
+	/// </summary>
+	/// <param name="color">カラー</param>
+	void Draw(unsigned int color = 0xff0000) const;
+};
+
+struct CircleCol
+{
+private:
+	// 当たり判定の半径
+	float radius;
+	// 中心座標
+	Pos3 pos;
+
+public:
+	float GetRadius() const { return radius; }
+	const Pos3& GetPos() const { return pos; }
+
+	/// <summary>
+	/// 半径の設定
+	/// </summary>
+	/// <param name="radius">半径</param>
+	void SetRadisu(float radius);
+	/// <summary>
+	/// 判定中心座標
+	/// </summary>
+	/// <param name="pos"></param>
+	void SetPos(const Pos3& pos);
+	
+	/// <summary>
+	/// 判定チェック
+	/// </summary>
+	/// <param name="col">判定対象</param>
+	/// <returns>true : 当たっている / false : 当たっていない</returns>
+	bool IsHit(const CircleCol& col);
+
+	/// <summary>
+	/// 判定場所描画
+	/// </summary>
+	/// <param name="color">カラー</param>
+	void Draw(unsigned int color = 0xff0000) const;
+};
